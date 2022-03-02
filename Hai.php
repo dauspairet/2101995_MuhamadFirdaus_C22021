67b76d92cd08a0c7a61e872e7c49cedc3f3766b7
mdaffayusuff <95558620+mdaffayusuff@users.noreply.github.com>
2022-03-02 18:09:37 +0700

    Hai halo

---
+++ Hai.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Halo Masyarakat Bumi</h1>
+
+    <?php
+    echo "ini adalah file yang dikirmkan demi memenuhi tugas RPL ";
+    echo "Muhammad Daffa Yusuf Fadhilah - 2100543 - C2 2021";?>
+
+  </body>
+</html>
